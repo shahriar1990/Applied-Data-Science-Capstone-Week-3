2cd74b7953d93a20ef54d02f269ff97404a08429
shahriar-sharifi <48140344+shahriar1990@users.noreply.github.com>
2021-05-24 13:53:12 +0430

    Update Segmenting and Clustering Neighborhoods in Toronto version 1.py

--- Segmenting and Clustering Neighborhoods in Toronto version 1.py
+++ Segmenting and Clustering Neighborhoods in Toronto version 1.py
@@ -1,1 +1,13 @@
 
+IBM Data Science Professional Certificate
+
+Applied Data Science Week 3 Project: Neighborhood Segmentation and Clustering
+
+Coursera: https://www.coursera.org/learn/applied-data-science-capstone
+
+Explore neighbourhood in Toronto
+
+Technique: Web scrapping, API, Clustering
+
+  
+  
